17c62aae9cf6f9f54509445f8988bd9dc0ad2442
Fabian Klemm <fabian.klemm@tngtech.com>
2024-04-27 00:08:01 +0200

    add formula

---
+++ Formula/rust-please.rb
@@ -0,0 +1,22 @@
+class RustPlease < Formula
+  desc "Please CLI (Rust port)"
+  homepage "https://github.com/fabiankle/rust-please"
+  url "https://github.com/fabiankle/rust-please/archive/refs/tags/0.2.2.zip", using: GitHubPrivateRepositoryDownloadStrategy
+  sha256 "7ea463a2a580ab5c1794571412d0c76b209aad91eb05fac2f63ad1992e6374bb"
+  head "https://github.com/fabiankle/rust-please.git"
+
+  depends_on "rust" => :build
+  conflicts_with "please", :because => "this is the rust port"
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/please" => "please"
+  end
+
+  test do
+    mkdir "repo" do
+      system "git", "init"
+      assert_equal "please-rust 0.2.2\n", shell_output("#{bin}/please --version")
+    end
+  end
+end
