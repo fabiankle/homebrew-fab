798fe679bf7ae528a89d9f78c0c2cf356bdc147f
Fabian Klemm <fabian.klemm@tngtech.com>
2024-04-29 21:26:31 +0200

    update

--- Formula/rust-please.rb
+++ Formula/rust-please.rb
@@ -4,11 +4,11 @@ require_relative "../custom_download_strategy"
 class RustPlease < Formula
   desc "Please CLI (Rust port)"
   homepage "https://github.com/fabiankle/rust-please"
-  url "https://api.github.com/repos/fabiankle/rust-please/zipball/v0.2.15", headers: [
+  url "https://api.github.com/repos/fabiankle/rust-please/zipball/v0.2.17", headers: [
                                                                                       "Accept: application/json",
                                                                                       "Authorization: Bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
                                                                                     ]
-  sha256 "57cf85d4110bba78e3d8ec3a290ac96e379cea2e8833244391e3b0aca4e40ebc"
+  sha256 "997274d322f9a88c04c6efcaac3c271a13702128b02a1fbbc7ba1574c1ea4513"
   head "https://github.com/fabiankle/rust-please.git"
 
   depends_on "rust" => :build
@@ -22,7 +22,7 @@ class RustPlease < Formula
   test do
     mkdir "repo" do
       system "git", "init"
-      assert_equal "please-rust 0.2.15\n", shell_output("#{bin}/please --version")
+      assert_equal "please-rust 0.2.17\n", shell_output("#{bin}/please --version")
     end
   end
 end
